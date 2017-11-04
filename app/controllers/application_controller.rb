f89c84586d6fe53cbd2ce7f3cde03bedc45babaf
Lauren <Laurenelee88@gmail.com>
2017-11-04 08:58:46 -0700

    needed to generate a songs controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::API
-  def index
-    songs = Song.all
-    render :json => songs.as_json(only: [:id, :title, :artist, :year]), status: :ok
-  end
+  
 
 end
